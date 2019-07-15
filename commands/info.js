94fe47008a5268a4812bf1cd89807907ccbfd808
Shinei <50706972+shineialter@users.noreply.github.com>
2019-07-15 10:11:18 +0700

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -10,6 +10,7 @@ module.exports.run = async (bot, message, args) => {
         .setThumbnail(boticon)
         .addField("Bot Name", bot.user.username)
         .addField("Developed By", "Shinei#7000")
+        .addField("Contributors", "Cronix#0363")
         .addField("Servers", `Currently online on ${bot.guilds.size} servers.`);
 
     message.channel.send({embed:botinfoembed});
