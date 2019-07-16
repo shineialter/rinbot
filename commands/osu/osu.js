d4aa2715cee218bc86493575a1861211445dcce4
Shinei <50706972+shineialter@users.noreply.github.com>
2019-07-16 16:17:28 +0700

    Update osu.js

--- commands/osu/osu.js
+++ commands/osu/osu.js
@@ -5,109 +5,153 @@ const fOsuUser = require("./utils/fOsuUser.js");
 
 module.exports.run = async (bot, message, args) => {
 
-        var osu = new nodeosu.Api(process.env.OSUAPI_TOKEN, {
-            notFoundAsError: true,
-            completeScores: false
-        });
+    var osu = new nodeosu.Api(process.env.OSUAPI_TOKEN, {
+        notFoundAsError: true,
+        completeScores: false
+    });
 
-        var uh = 0
-        var osuIcon = `https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Osu%21Logo_%282015%29.svg/330px-Osu%21Logo_%282015%29.svg.png`
+    var uh = 0
+    var osuIcon = `https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Osu%21Logo_%282015%29.svg/330px-Osu%21Logo_%282015%29.svg.png`
 
-        if (args[0]) {
+    var getUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
-            var findPlayer = args[0];
+    if (getUser) {
 
-            if (message.content.includes('"')) {
+        fOsuUser.otherUser(Osu_User, getUser, async (res_othuser) => {
 
-                var spaces = message.content.split('"')
-                var spacedname = spaces[1]
+            if (!res_othuser) {
+
+                let mentionNotFound = new Discord.RichEmbed()
+                    .setAuthor(`This person has no osu! Standard profile linked to their discord account!`, `${osuIcon}`)
+                    .setColor("#f2873f");
+
+                message.channel.send({embed:mentionNotFound})
             }
-            
-            else {
 
-                spacedname = findPlayer;
-            }
-            console.log(findPlayer)
-            try {
-                
-                var osugetuser = await osu.getUser({u: `${spacedname}`});
-                var userid = osugetuser.id;
-                var userosuname = osugetuser.name;
-                var usercountry = osugetuser.country;
-                var userpp = osugetuser.pp.raw;
-                var userglobalrank = osugetuser.pp.rank;
-                var userlocalrank = osugetuser.pp.countryRank;
-                var userlevel = osugetuser.level;
-                var useracc = osugetuser.accuracy;
-                var userplaycount = osugetuser.counts.plays;
-                var useravatar = `https://a.ppy.sh/${userid}?date=${uh}`
-                var userlink = `https://osu.ppy.sh/users/${userid}`
+            else if (res_othuser) {
 
-                if (!userosuname) {
+                var mentioneduser = res_othuser.osu_username;
+                console.log(mentioneduser);
+                var mentiongetuser = await osu.getUser({u: mentioneduser});
+                var mentionid = mentiongetuser.id;
+                var mentionosuname = mentiongetuser.name;
+                var mentioncountry = mentiongetuser.country;
+                var mentionpp = mentiongetuser.pp.raw;
+                var mentionglobalrank = mentiongetuser.pp.rank;
+                var mentionlocalrank = mentiongetuser.pp.countryRank;
+                var mentionlevel = mentiongetuser.level;
+                var mentionacc = mentiongetuser.accuracy;
+                var mentionplaycount = mentiongetuser.counts.plays;
+                var mentionavatar = `https://a.ppy.sh/${mentionid}?date=${uh}`
+                var mentionlink = `https://osu.ppy.sh/users/${mentionid}`
 
-                    throw err;
-                }
-
-                let userEmb = new Discord.RichEmbed()
-                    .setAuthor(`${userosuname}'s osu! Standard Profile`, `${osuIcon}`, userlink)
-                    .setDescription(`**Global Rank** ⬞ #${Number(userglobalrank).toLocaleString()} ⬞ :flag_${usercountry.toLowerCase()}: #${Number(userlocalrank).toLocaleString()}\n**PP** ⬞ ${Number(userpp).toFixed(0)}\n**Level** ⬞ ${Number(userlevel).toFixed(0)}\n**Accuracy** ⬞ ${Number(useracc).toFixed(2)}%\n**Play Count** ⬞ ${Number(userplaycount).toLocaleString()}`)
-                    .setThumbnail(useravatar)
+                let mentionEmb = new Discord.RichEmbed()
+                    .setAuthor(`${mentionosuname}'s osu! Standard Profile`, `${osuIcon}`, mentionlink)
+                    .setDescription(`**Global Rank** ⬞ #${Number(mentionglobalrank).toLocaleString()} ⬞ :flag_${mentioncountry.toLowerCase()}: #${Number(mentionlocalrank).toLocaleString()}\n**PP** ⬞ ${Number(mentionpp).toFixed(0)}\n**Level** ⬞ ${Number(mentionlevel).toFixed(0)}\n**Accuracy** ⬞ ${Number(mentionacc).toFixed(2)}%\n**Play Count** ⬞ ${Number(mentionplaycount).toLocaleString()}`)
+                    .setThumbnail(mentionavatar)
                     .setColor("#f2873f")
                     .setFooter(`▹ Live from osu! server`)
 
-                message.channel.send({embed:userEmb})
+                message.channel.send({embed:mentionEmb})
             }
+        })   
+    }
 
-            catch (err) {
+    else if (args[0]) {
 
-                let uNotEmb = new Discord.RichEmbed()
-                .setAuthor(`Can't find username!`, `${osuIcon}`)
-                .setColor("#f2873f");
+        var findPlayer = args[0];
 
-            message.channel.send({embed:uNotEmb})
-            return;
-            }
+        if (message.content.includes('"')) {
+
+            var spaces = message.content.split('"')
+            var spacedname = spaces[1]
         }
-
+        
         else {
 
-            fOsuUser.myself(Osu_User, message, async (res_user) => {
-                if (!res_user) {
-    
-                    let noUserEmb = new Discord.RichEmbed()
-                    .setAuthor(`No username linked to your discord account!`, `${osuIcon}`)
-                    .setDescription(`⬞⬞▹ Use  **?osuset user <username>**  to do so.`)
-                    .setColor("#f2873f");
-    
-                    message.channel.send({embed:noUserEmb})
-                }
-    
-                else {
-    
-                    var playerData = res_user.osu_username;
-                    var osugetplayer = await osu.getUser({u: playerData});
-                    var playername = osugetplayer.name;
-                    var playerid = osugetplayer.id;
-                    var playercountry = osugetplayer.country;
-                    var playerpp = osugetplayer.pp.raw;
-                    var playerglobalrank = osugetplayer.pp.rank;
-                    var playerlocalrank = osugetplayer.pp.countryRank;
-                    var playerlevel = osugetplayer.level;
-                    var playeracc = osugetplayer.accuracy;
-                    var playerplaycount = osugetplayer.counts.plays;
-                    var playeravatar = `https://a.ppy.sh/${playerid}?date=${uh}`
-                    var playerlink = `https://osu.ppy.sh/users/${playerid}`
-    
-                    let playerEmb = new Discord.RichEmbed()
-                        .setAuthor(`${playername}'s osu! Standard Profile`, `${osuIcon}`, playerlink)
-                        .setDescription(`**Global Rank** ⬞ #${Number(playerglobalrank).toLocaleString()} ⬞ :flag_${playercountry.toLowerCase()}: #${Number(playerlocalrank).toLocaleString()}\n**PP** ⬞ ${Number(playerpp).toFixed(0)}\n**Level** ⬞ ${Number(playerlevel).toFixed(0)}\n**Accuracy** ⬞ ${Number(playeracc).toFixed(2)}%\n**Play Count** ⬞ ${Number(playerplaycount).toLocaleString()}`)
-                        .setThumbnail(playeravatar)
-                        .setColor("#f2873f")
-                        .setFooter(`▹ Live from osu! server`)
-    
-                    message.channel.send({embed:playerEmb})
-                }
-            })
+            spacedname = findPlayer;
+        }
+
+        try {
+            
+            var osugetuser = await osu.getUser({u: `${spacedname}`});
+            var userid = osugetuser.id;
+            var userosuname = osugetuser.name;
+            var usercountry = osugetuser.country;
+            var userpp = osugetuser.pp.raw;
+            var userglobalrank = osugetuser.pp.rank;
+            var userlocalrank = osugetuser.pp.countryRank;
+            var userlevel = osugetuser.level;
+            var useracc = osugetuser.accuracy;
+            var userplaycount = osugetuser.counts.plays;
+            var useravatar = `https://a.ppy.sh/${userid}?date=${uh}`
+            var userlink = `https://osu.ppy.sh/users/${userid}`
+
+            if (!userosuname) {
+
+                throw err;
+            }
+
+            let userEmb = new Discord.RichEmbed()
+                .setAuthor(`${userosuname}'s osu! Standard Profile`, `${osuIcon}`, userlink)
+                .setDescription(`**Global Rank** ⬞ #${Number(userglobalrank).toLocaleString()} ⬞ :flag_${usercountry.toLowerCase()}: #${Number(userlocalrank).toLocaleString()}\n**PP** ⬞ ${Number(userpp).toFixed(0)}\n**Level** ⬞ ${Number(userlevel).toFixed(0)}\n**Accuracy** ⬞ ${Number(useracc).toFixed(2)}%\n**Play Count** ⬞ ${Number(userplaycount).toLocaleString()}`)
+                .setThumbnail(useravatar)
+                .setColor("#f2873f")
+                .setFooter(`▹ Live from osu! server`)
+
+            message.channel.send({embed:userEmb})
+        }
+
+        catch (err) {
+
+            let uNotEmb = new Discord.RichEmbed()
+            .setAuthor(`Can't find username!`, `${osuIcon}`)
+            .setColor("#f2873f");
+
+        message.channel.send({embed:uNotEmb})
+        return;
         }    
     }
+
+    else {
+
+        fOsuUser.myself(Osu_User, message, async (res_user) => {
+            if (!res_user) {
+
+                let noUserEmb = new Discord.RichEmbed()
+                .setAuthor(`No username linked to your discord account!`, `${osuIcon}`)
+                .setDescription(`⬞⬞▹ Use  **?osuset user <username>**  to do so.`)
+                .setColor("#f2873f");
+
+                message.channel.send({embed:noUserEmb})
+            }
+
+            else {
+
+                var playerData = res_user.osu_username;
+                var osugetplayer = await osu.getUser({u: playerData});
+                var playername = osugetplayer.name;
+                var playerid = osugetplayer.id;
+                var playercountry = osugetplayer.country;
+                var playerpp = osugetplayer.pp.raw;
+                var playerglobalrank = osugetplayer.pp.rank;
+                var playerlocalrank = osugetplayer.pp.countryRank;
+                var playerlevel = osugetplayer.level;
+                var playeracc = osugetplayer.accuracy;
+                var playerplaycount = osugetplayer.counts.plays;
+                var playeravatar = `https://a.ppy.sh/${playerid}?date=${uh}`
+                var playerlink = `https://osu.ppy.sh/users/${playerid}`
+
+                let playerEmb = new Discord.RichEmbed()
+                    .setAuthor(`${playername}'s osu! Standard Profile`, `${osuIcon}`, playerlink)
+                    .setDescription(`**Global Rank** ⬞ #${Number(playerglobalrank).toLocaleString()} ⬞ :flag_${playercountry.toLowerCase()}: #${Number(playerlocalrank).toLocaleString()}\n**PP** ⬞ ${Number(playerpp).toFixed(0)}\n**Level** ⬞ ${Number(playerlevel).toFixed(0)}\n**Accuracy** ⬞ ${Number(playeracc).toFixed(2)}%\n**Play Count** ⬞ ${Number(playerplaycount).toLocaleString()}`)
+                    .setThumbnail(playeravatar)
+                    .setColor("#f2873f")
+                    .setFooter(`▹ Live from osu! server`)
+
+                message.channel.send({embed:playerEmb})
+            }
+        })
+    }    
+}
 
