567983da25b0a1ccc27dbffa4966ae95ea154c58
Shinei <50706972+shineialter@users.noreply.github.com>
2019-07-19 13:26:56 +0700

    Add files via upload

--- commands/osu/utils/fStdBm.js
+++ commands/osu/utils/fStdBm.js
@@ -3,8 +3,25 @@ module.exports = {
         osu_beatmap.find({
             approved: 1 || 4,
             difficultyrating: {
-                $gte: 4, 
-                $lte: 5
+                $gte: 3.2, 
+                $lte: 4.2
+            }
+        }, (err, res_map) => {
+            if (err) throw err;
+            if (!res_map) {
+                callback(res_map);
+            } else {
+                callback(res_map)
+            }
+        })
+    },
+
+    questmedium: (osu_beatmap, message, callback) => {
+        osu_beatmap.find({
+            approved: 1 || 4,
+            difficultyrating: {
+                $gte: 4.2, 
+                $lte: 5.4
             }
         }, (err, res_map) => {
             if (err) throw err;
